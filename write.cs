94d4067708273e4eb2b2cd0846129b55faa437a8
Enio Fernandes <EN10@users.noreply.github.com>
2023-12-09 14:22:15 +0000

    Create write.cs

---
+++ write.cs
@@ -0,0 +1,14 @@
+// StreamWriter Line Example 
+using System; 
+using System.IO; 
+namespace WriteFile 
+{    
+    class Write {          
+        static void Main(string[] args) 
+        { 
+         	StreamWriter fileOut = new StreamWriter("fileName.txt"); 
+          fileOut.WriteLine("testing123"); 
+          fileOut.Close(); 
+        } 
+    } 
+} 
